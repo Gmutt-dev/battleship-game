24f9eb73322df274e1253a24b9781159188f661a
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-12-18 16:02:14 +0200

    changed import to only use the Ship class as a type
    Class not required for instances at this time.  For clarity.

--- src/models/GameBoard.ts
+++ src/models/GameBoard.ts
@@ -1,5 +1,5 @@
 import { GridBlock } from "./GridBlock";
-import { Ship } from "./Ship";
+import { type Ship } from "./Ship";
 
 type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 type TwoDigits = `${Digit}${Digit}`;
