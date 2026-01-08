3c931b298fe6840d4380276346f43a6fc6b71f48
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2026-01-08 15:59:56 +0200

    added an initially created empty gameboard when a player is created

--- src/models/Player.test.ts
+++ src/models/Player.test.ts
@@ -2,14 +2,10 @@ import { it, expect } from "vitest";
 import { Player } from "./Player";
 
 it("can create a human player called John", () => {
-  expect(new Player({ name: "John", type: "human" })).toEqual({
-    name: "John",
-    type: "human",
-  });
+  expect(new Player({ name: "John", type: "human" })).toBeInstanceOf(Player);
 });
 it("can create a computer player called SuperDuper", () => {
-  expect(new Player({ name: "SuperDuper", type: "computer" })).toEqual({
-    name: "SuperDuper",
-    type: "computer",
-  });
+  expect(new Player({ name: "SuperDuper", type: "computer" })).toBeInstanceOf(
+    Player
+  );
 });
--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,9 +1,13 @@
+import { GameBoard } from "./GameBoard";
+
 export class Player {
   public name: string;
   public type: "human" | "computer";
+  public gameBoard: GameBoard;
 
   constructor({ name, type }: { name: string; type: "human" | "computer" }) {
     this.name = name;
     this.type = type;
+    this.gameBoard = new GameBoard();
   }
 }
