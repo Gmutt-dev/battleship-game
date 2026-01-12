5a702d1366513cd4ca9bc26fd0da0015572082cb
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2026-01-12 15:11:12 +0200

    updated with tests

--- src/models/GridBlock.test.ts
+++ src/models/GridBlock.test.ts
@@ -1,6 +1,57 @@
-import { expect, it } from "vitest";
-import { GridBlock } from "./GridBlock";
+import { describe, expect, it } from "vitest";
+import { TargetGridBlock, OceanGridBlock } from "./GridBlock";
+import { Ship } from "./Ship";
 
-it("can create object instance", () => {
-  expect(new GridBlock()).toBeInstanceOf(GridBlock);
+describe("TargetGridBlock tests:", () => {
+  it("can create object instance", () => {
+    expect(new TargetGridBlock("A1")).toBeInstanceOf(TargetGridBlock);
+  });
+  it("can return string column coordinate", () => {
+    expect(new TargetGridBlock("A1").columnCoordinate).toEqual("A");
+  });
+  it("can return string row coordinate", () => {
+    expect(new TargetGridBlock("A10").rowCoordinate).toEqual("10");
+  });
+  it("can mark an attack on a gridblock when no ship is hit", () => {
+    const testGridBlock = new TargetGridBlock("A10");
+    expect(testGridBlock.isAttacked).toEqual(false);
+    testGridBlock.markAttack();
+    expect(testGridBlock.isAttacked).toEqual(true);
+    expect(testGridBlock.containsShipSegmentOf).toEqual(null);
+  });
+  it("can mark an attack and keep track of a ship that was hit", () => {
+    const testGridBlock = new TargetGridBlock("A10");
+    const testShip = new Ship({ type: "Battleship", segments: 4 });
+    testGridBlock.markAttack(testShip);
+    expect(testGridBlock.isAttacked).toEqual(true);
+    expect(testGridBlock.containsShipSegmentOf).toEqual(testShip);
+  });
+});
+
+describe("OceanGridBlock tests:", () => {
+  it("can create object instance", () => {
+    expect(new OceanGridBlock("A1")).toBeInstanceOf(OceanGridBlock);
+  });
+  it("can return string column coordinate", () => {
+    expect(new OceanGridBlock("A1").columnCoordinate).toEqual("A");
+  });
+  it("can return string row coordinate", () => {
+    expect(new OceanGridBlock("A10").rowCoordinate).toEqual("10");
+  });
+  it("can receive an attack and mark itself as attacked", () => {
+    const testGridBlock = new OceanGridBlock("A10");
+    expect(testGridBlock.isAttacked).toEqual(false);
+    testGridBlock.receiveAttack();
+    expect(testGridBlock.isAttacked).toEqual(true);
+  });
+  it("can return null if attacked and there is no ship segment present", () => {
+    const testGridBlock = new OceanGridBlock("A10");
+    expect(testGridBlock.receiveAttack()).toEqual(null);
+  });
+  it("can return a ship reference if attacked and a ship segment is present", () => {
+    const testGridBlock = new OceanGridBlock("A10");
+    const testShip = new Ship({ type: "Battleship", segments: 4 });
+    testGridBlock.containsShipSegmentOf = testShip;
+    expect(testGridBlock.receiveAttack()).toEqual(testShip);
+  });
 });
--- src/models/GridBlock.ts
+++ src/models/GridBlock.ts
@@ -1,5 +1,46 @@
-import type { Ship } from "./Ship";
+import { type Ship } from "./Ship";
+import {
+  type StringCoordinates,
+  type ColumnLetter,
+  type RowNumber,
+  extractColumnAndRowCoordinates,
+} from "./GameBoard";
 
-export class GridBlock {
+class GridBlock {
+  public coordinates: StringCoordinates;
+  public isAttacked: boolean = false;
   public containsShipSegmentOf: Ship | null = null;
+
+  constructor(coordinates: StringCoordinates) {
+    this.coordinates = coordinates;
+  }
+
+  public get columnCoordinate(): ColumnLetter {
+    return extractColumnAndRowCoordinates(this.coordinates)[0];
+  }
+
+  public get rowCoordinate(): RowNumber {
+    return extractColumnAndRowCoordinates(this.coordinates)[1];
+  }
+}
+
+export class TargetGridBlock extends GridBlock {
+  constructor(coordinates: StringCoordinates) {
+    super(coordinates);
+  }
+  public markAttack(ship: Ship | null = null) {
+    this.isAttacked = true;
+    this.containsShipSegmentOf = ship;
+  }
+}
+
+export class OceanGridBlock extends GridBlock {
+  constructor(coordinates: StringCoordinates) {
+    super(coordinates);
+  }
+
+  public receiveAttack() {
+    this.isAttacked = true;
+    return this.containsShipSegmentOf; // return the ship or null so that controller can notify opponent of what ship was hit (or it was a miss)
+  }
 }
