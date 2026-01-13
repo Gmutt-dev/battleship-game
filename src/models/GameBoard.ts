8d606dca569eb73e9a0f4b36bdac45bcaaffe6cb
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2026-01-13 12:20:54 +0200

    completed Gameboard .receiveAttack() and .markAttack() with tests

--- src/models/GameBoard.ts
+++ src/models/GameBoard.ts
@@ -1,101 +1,128 @@
-import { GridBlock } from "./GridBlock";
-import { type Ship } from "./Ship";
+import { TargetGridBlock, OceanGridBlock } from "./GridBlock";
+import { Ship } from "./Ship";
 
-type ColumnLetter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J";
-type RowNumber = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10";
-type StringCoordinates = `${ColumnLetter}${RowNumber}`;
+export type ColumnLetter =
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "H"
+  | "I"
+  | "J";
+export type RowNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+export type Coordinates = { columnLetter: ColumnLetter; rowNumber: RowNumber };
 
 type ShipAlignment = "horizontal" | "vertical";
 
 const [GRID_COLUMNS, GRID_ROWS] = [10, 10];
-
-function extractRowAndColumnCoordinates(
-  coordinates: StringCoordinates
-): number[] {
-  const columnLetters = "ABCDEFGHIJ";
-  return [
-    columnLetters.indexOf(coordinates[0]),
-    Number.parseInt(coordinates.slice(1)),
-  ];
-}
-
-function isPlaceable({
-  ship,
-  rowCoordinateStart,
-  columnCoordinateStart,
-  alignment,
-}: {
-  ship: Ship;
-  rowCoordinateStart: number;
-  columnCoordinateStart: number;
-  alignment: ShipAlignment;
-}): boolean {
-  const maxPositions = alignment === "horizontal" ? GRID_COLUMNS : GRID_ROWS;
-  const startPosition =
-    alignment === "horizontal" ? columnCoordinateStart : rowCoordinateStart;
-  const availablePositions = maxPositions - startPosition;
-
-  return ship.length <= availablePositions;
-}
+const COLUMN_LETTERS = "ABCDEFGHIJ";
 
 export class GameBoard {
-  private _playerGrid: GridBlock[][] = Array.from({ length: GRID_ROWS }, () =>
-    Array.from({ length: GRID_COLUMNS }, () => new GridBlock())
-  );
+  public readonly targetGrid: ReadonlyArray<ReadonlyArray<TargetGridBlock>>;
+  public readonly oceanGrid: ReadonlyArray<ReadonlyArray<OceanGridBlock>>;
 
-  private _opponentGrid: GridBlock[][] = Array.from({ length: GRID_ROWS }, () =>
-    Array.from({ length: GRID_COLUMNS }, () => new GridBlock())
-  );
-  //   constructor({}: {}) {}
+  constructor() {
+    this.targetGrid = Array.from({ length: GRID_COLUMNS }, (v, columnIndex) =>
+      Array.from({ length: GRID_ROWS }, (v, rowIndex) => {
+        const coordinatesFromArrayIndexes: Coordinates = {
+          columnLetter: COLUMN_LETTERS[columnIndex] as ColumnLetter,
+          rowNumber: (rowIndex + 1) as RowNumber,
+        };
+        return new TargetGridBlock(coordinatesFromArrayIndexes);
+      })
+    );
+    this.oceanGrid = Array.from({ length: GRID_COLUMNS }, (v, columnIndex) =>
+      Array.from({ length: GRID_ROWS }, (v, rowIndex) => {
+        const coordinatesFromArrayIndexes: Coordinates = {
+          columnLetter: COLUMN_LETTERS[columnIndex] as ColumnLetter,
+          rowNumber: (rowIndex + 1) as RowNumber,
+        };
+        return new OceanGridBlock(coordinatesFromArrayIndexes);
+      })
+    );
+  }
 
   public placeShip({
-    ship,
+    shipDetails,
     coordinatesStart,
     alignment = "horizontal",
   }: {
-    ship: Ship;
-    coordinatesStart: StringCoordinates;
+    shipDetails: { type: string; segments: number };
+    coordinatesStart: Coordinates;
     alignment: ShipAlignment;
-  }): Ship {
-    const [rowCoordinateStart, columnCoordinateStart] =
-      extractRowAndColumnCoordinates(coordinatesStart);
-
-    if (
-      !isPlaceable({
-        ship,
-        rowCoordinateStart,
-        columnCoordinateStart,
-        alignment,
-      })
-    )
-      return this;
-
-    if (alignment === "horizontal") {
-      for (
-        let selectedColumn = columnCoordinateStart;
-        selectedColumn < columnCoordinateStart + ship.length;
-        selectedColumn++
-      ) {
-        this._playerGrid[rowCoordinateStart][
-          selectedColumn
-        ].containsShipSegmentOf = ship;
-      }
+  }): GameBoard {
+    const startingGridBlock = this.oceanGrid
+      .flat()
+      .find(
+        (gridBlock) =>
+          gridBlock.coordinates.columnLetter ===
+            coordinatesStart.columnLetter &&
+          gridBlock.coordinates.rowNumber === coordinatesStart.rowNumber
+      );
+    if (startingGridBlock === undefined) return this;
+    let placementGridBlocks;
+    if (alignment === "vertical") {
+      const placementColumn = this.oceanGrid.find((column) =>
+        column.some((gridBlock) => gridBlock === startingGridBlock)
+      );
+      if (placementColumn === undefined) return this;
+      placementGridBlocks = placementColumn.slice(
+        placementColumn.indexOf(startingGridBlock),
+        placementColumn.indexOf(startingGridBlock) + shipDetails.segments
+      );
     } else {
-      for (
-        let selectedRow = rowCoordinateStart;
-        selectedRow < rowCoordinateStart + ship.length;
-        selectedRow++
-      ) {
-        this._playerGrid[selectedRow][
-          columnCoordinateStart
-        ].containsShipSegmentOf = ship;
-      }
+      // alignment === "horizontal"
+      const placementRow = this.oceanGrid
+        .flat()
+        .filter(
+          (gridBlock) =>
+            gridBlock.coordinates.rowNumber === coordinatesStart.rowNumber
+        );
+      placementGridBlocks = placementRow.slice(
+        placementRow.indexOf(startingGridBlock),
+        placementRow.indexOf(startingGridBlock) + shipDetails.segments
+      );
     }
 
+    if (placementGridBlocks.length < shipDetails.segments) return this; // placed ship shouldn't go outside gameboard
+    if (
+      placementGridBlocks?.some(
+        (gridBlock) => gridBlock.containsShipSegmentOf !== null
+      )
+    )
+      return this; // placed ship should not collide with already placed ship
+    //place ship refs
+    const shipToPlace = new Ship(shipDetails);
+    placementGridBlocks.forEach(
+      (gridBlock) => (gridBlock.containsShipSegmentOf = shipToPlace)
+    );
     return this;
   }
 
-  public receiveAttack(coordinates: StringCoordinates): GameBoard {
+  public receiveAttack(coordinates: Coordinates): Ship | null {
+    return this.oceanGrid
+      .flat()
+      .find(
+        (gridBlock) =>
+          gridBlock.coordinates.columnLetter === coordinates.columnLetter &&
+          gridBlock.coordinates.rowNumber === coordinates.rowNumber
+      )
+      .receiveAttack();
+  }
+
+  public markAttack(coordinates: Coordinates, hitShip: Ship | null): GameBoard {
+    this.targetGrid
+      .flat()
+      .find(
+        (gridBlock) =>
+          gridBlock.coordinates.columnLetter === coordinates.columnLetter &&
+          gridBlock.coordinates.rowNumber === coordinates.rowNumber
+      )
+      .markAttack(hitShip);
+
     return this;
   }
 }
--- src/models/Gameboard.test.ts
+++ src/models/Gameboard.test.ts
@@ -1,58 +1,163 @@
-import { expect, it } from "vitest";
-import { GameBoard } from "./GameBoard.ts";
+import { describe, expect, it, vi } from "vitest";
+import { GameBoard, type Coordinates } from "./GameBoard.ts";
+import { OceanGridBlock } from "./GridBlock";
+import { Ship } from "./Ship.ts";
+
+const coordinatesA1: Coordinates = { columnLetter: "A", rowNumber: 1 };
+const coordinatesJ10: Coordinates = { columnLetter: "J", rowNumber: 10 };
+const coordinatesH1: Coordinates = { columnLetter: "H", rowNumber: 1 };
+const coordinatesB8: Coordinates = { columnLetter: "B", rowNumber: 8 };
+const coordinatesB1: Coordinates = { columnLetter: "B", rowNumber: 1 };
+const coordinatesA2: Coordinates = { columnLetter: "A", rowNumber: 2 };
 
 it("can create instance of", () => {
   expect(new GameBoard()).toBeInstanceOf(GameBoard);
 });
+it("has an oceangrid that contains GridBlock instances in all of it's elements", () => {
+  expect(
+    new GameBoard().oceanGrid
+      .flat()
+      .every((element) => element instanceof OceanGridBlock)
+  ).toEqual(true);
+});
+it("has an oceangrid with a first element with coordinates of A1", () => {
+  expect(new GameBoard().oceanGrid.flat()[0].coordinates).toEqual(
+    coordinatesA1
+  );
+});
+it("has an oceangrid with a last element with coordinates of J10", () => {
+  const testOceanGrid = new GameBoard().oceanGrid;
+  expect(
+    testOceanGrid.flat()[testOceanGrid.flat().length - 1].coordinates
+  ).toEqual(coordinatesJ10);
+});
 
-// tests that cant run on public interface commented out
+it("can place a ship on the player grid at top left A1 coordinates horizontally", () => {
+  const testGameboard = new GameBoard();
+  const testShipDetail = { type: "Battleship", segments: 4 };
+  const coordinatesStart = coordinatesA1;
+  const alignment = "horizontal";
+  testGameboard.placeShip({
+    shipDetails: testShipDetail,
+    coordinatesStart,
+    alignment,
+  });
+  expect(testGameboard.oceanGrid[0][0].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[1][0].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[2][0].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[3][0].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[4][0].containsShipSegmentOf).toBe(null);
+  expect(
+    testGameboard.oceanGrid[4][0].containsShipSegmentOf
+  ).not.toBeInstanceOf(Ship);
+  expect(testGameboard.oceanGrid[5][0].containsShipSegmentOf).toBe(null);
+});
 
-// it("can place a ship on the player grid at top left 00 coordinates horizontally", () => {
-//   const ship = new Ship({ length: 5 });
-//   const coordinateStart = "00";
-//   const alignment = "horizontal";
-//   testGameboard.placeShip({ ship, coordinateStart, alignment });
-//   expect(testGameboard.playerGrid[0][0].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[0][1].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[0][2].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[0][3].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[0][4].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[0][5].containsShipSegmentOf).not.toBe(ship);
-//   expect(testGameboard.playerGrid[0][6].containsShipSegmentOf).toBe(null);
-// });
+it("can place a ship on the player grid at top left A1 coordinates vertically", () => {
+  const testGameboard = new GameBoard();
+  const testShipDetail = { type: "Battleship", segments: 4 };
+  const coordinatesStart = coordinatesA1;
+  const alignment = "vertical";
+  testGameboard.placeShip({
+    shipDetails: testShipDetail,
+    coordinatesStart,
+    alignment,
+  });
+  expect(testGameboard.oceanGrid[0][0].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[0][1].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[0][2].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[0][3].containsShipSegmentOf).toBeInstanceOf(
+    Ship
+  );
+  expect(testGameboard.oceanGrid[0][4].containsShipSegmentOf).toBe(null);
+  expect(
+    testGameboard.oceanGrid[0][4].containsShipSegmentOf
+  ).not.toBeInstanceOf(Ship);
+  expect(testGameboard.oceanGrid[0][5].containsShipSegmentOf).toBe(null);
+});
 
-// it("can place a ship on the player grid at top left 00 coordinates vertically", () => {
-//   const ship = new Ship({ length: 5 });
-//   const coordinateStart = "00";
-//   const alignment = "vertical";
-//   testGameboard.placeShip({ ship, coordinateStart, alignment });
-//   expect(testGameboard.playerGrid[0][0].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[1][0].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[2][0].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[3][0].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[4][0].containsShipSegmentOf).toBe(ship);
-//   expect(testGameboard.playerGrid[5][0].containsShipSegmentOf).not.toBe(ship);
-//   expect(testGameboard.playerGrid[6][0].containsShipSegmentOf).toBe(null);
-// });
+it("WILL NOT place a ship, so that it will go outside of the gameboard horizontally - i.e. placement cancelled", () => {
+  const testGameboard = new GameBoard();
+  const testShipDetail = { type: "Battleship", segments: 4 };
+  const coordinatesStart = coordinatesH1;
+  const alignment = "horizontal";
+  testGameboard.placeShip({
+    shipDetails: testShipDetail,
+    coordinatesStart,
+    alignment,
+  });
+  expect(testGameboard.oceanGrid[7][0].containsShipSegmentOf).toBe(null);
+  expect(testGameboard.oceanGrid[8][0].containsShipSegmentOf).toBe(null);
+  expect(testGameboard.oceanGrid[9][0].containsShipSegmentOf).toBe(null);
+});
 
-// it("WILL NOT place a ship, so that it will go outside of the gameboard horizontally - i.e. placement cancelled", () => {
-//   const ship = new Ship({ length: 5 });
-//   const coordinateStart = "08";
-//   const alignment = "horizontal";
-//   testGameboard.placeShip({ ship, coordinateStart, alignment });
-//   expect(testGameboard.playerGrid[0][8].containsShipSegmentOf).toBe(null);
-//   expect(testGameboard.playerGrid[0][9].containsShipSegmentOf).toBe(null);
-// });
+it("WILL NOT place a ship, so that it will go outside of the gameboard vertically - i.e. placement cancelled", () => {
+  const testGameboard = new GameBoard();
+  const testShipDetail = { type: "Battleship", segments: 4 };
+  const coordinatesStart = coordinatesB8;
+  const alignment = "vertical";
+  testGameboard.placeShip({
+    shipDetails: testShipDetail,
+    coordinatesStart,
+    alignment,
+  });
+  expect(testGameboard.oceanGrid[1][6].containsShipSegmentOf).toBe(null);
+  expect(testGameboard.oceanGrid[1][7].containsShipSegmentOf).toBe(null);
+  expect(testGameboard.oceanGrid[1][8].containsShipSegmentOf).toBe(null);
+  expect(testGameboard.oceanGrid[1][9].containsShipSegmentOf).toBe(null);
+});
 
-// it("WILL NOT place a ship, so that it will go outside of the gameboard vertically - i.e. placement cancelled", () => {
-//   const ship = new Ship({ length: 5 });
-//   const coordinateStart = "85";
-//   const alignment = "vertical";
-//   testGameboard.placeShip({ ship, coordinateStart, alignment });
-//   expect(testGameboard.playerGrid[8][5].containsShipSegmentOf).toBe(null);
-//   expect(testGameboard.playerGrid[9][5].containsShipSegmentOf).not.toBe(ship);
-// });
+it("won't place ship if it will collide with another already placed ship -> cancel placement and return gameboard without changes", () => {
+  const testGameboard = new GameBoard();
+  const testShipDetail1 = { type: "Battleship", segments: 4 };
+  const coordinatesStart1 = coordinatesB1;
+  const alignment1 = "vertical";
+  testGameboard.placeShip({
+    shipDetails: testShipDetail1,
+    coordinatesStart: coordinatesStart1,
+    alignment: alignment1,
+  });
+  const oceanGridSnapshot = structuredClone(testGameboard.oceanGrid);
+  const testShipDetail2 = { type: "Cruiser", segments: 3 };
+  const coordinatesStart2 = coordinatesA2;
+  const alignment2 = "horizontal";
+  expect(
+    testGameboard.placeShip({
+      shipDetails: testShipDetail2,
+      coordinatesStart: coordinatesStart2,
+      alignment: alignment2,
+    }).oceanGrid
+  ).toEqual(oceanGridSnapshot);
+});
 
-// need to (1) handle letter-number coords (2) alignment (2.1) for number of grid squares
-//check that ship placement does not collide with already placed ship
-//is the class of ship already placed? Only one of each class allowed
+describe("Relay tests:", () => {
+  const testGameboardOfActivePlayer = new GameBoard();
+  const testGameboardOfOpponentPlayer = new GameBoard();
+  let attackedShip: Ship | null;
+  it("can relay a received attack onward to the oceangridblock' method for handling", () => {
+    const spy = vi.spyOn(testGameboardOfOpponentPlayer, "receiveAttack");
+    attackedShip = testGameboardOfOpponentPlayer.receiveAttack(coordinatesA1);
+    expect(spy).toReturn();
+    spy.mockRestore();
+  });
+  it("can send a mark attack onward to the targetgrid method for handling", () => {
+    const spy = vi.spyOn(testGameboardOfActivePlayer, "markAttack");
+    testGameboardOfActivePlayer.markAttack(coordinatesA1, attackedShip);
+    expect(spy).toReturn();
+    spy.mockRestore();
+  });
+});
