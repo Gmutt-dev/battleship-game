a854d8aeb44c65ceb1c1852428ceb8b50269908c
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2026-01-22 15:08:45 +0200

    created test to check that a gameboard is created when a player is created

--- src/models/Player.test.ts
+++ src/models/Player.test.ts
@@ -1,5 +1,6 @@
 import { it, expect } from "vitest";
 import { Player } from "./Player";
+import { GameBoard } from "./GameBoard";
 
 it("can create a human player called John", () => {
   expect(new Player({ name: "John", type: "human" })).toBeInstanceOf(Player);
@@ -7,5 +8,10 @@ it("can create a human player called John", () => {
 it("can create a computer player called SuperDuper", () => {
   expect(new Player({ name: "SuperDuper", type: "computer" })).toBeInstanceOf(
     Player
+  );
+});
+it("creates a default gameboard with a new player created", () => {
+  expect(new Player({ name: "John", type: "human" }).gameBoard).toBeInstanceOf(
+    GameBoard
   );
 });
