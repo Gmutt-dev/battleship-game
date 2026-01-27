1a6beccdccc329e0d4d3fb08e5065efe485c858f
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2026-01-27 14:47:28 +0200

    removed comment

--- src/controllers/GameController.test.ts
+++ src/controllers/GameController.test.ts
@@ -2,10 +2,6 @@ import { it, expect } from "vitest";
 import { Player } from "../models/Player";
 import { GameController } from "./GameController";
 
-// it("can intantiate and export a single instance of GameController", () => {
-//   expect(gameController).toBeInstanceOf(Object);
-// });
-
 it("can create a new human player named John and have it referenced in activePlayer property of gameBoard AND can create a second computer player named SuperDuper and have it referenced by opponentPlayer", () => {
   const gameController = new GameController({
     player1Details: { name: "John", type: "human" },
